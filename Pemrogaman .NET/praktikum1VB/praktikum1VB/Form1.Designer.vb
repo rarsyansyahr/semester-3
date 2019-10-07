a5cc62245f7317910e254d7114745c7d5713d3dd
Rizky Arsyansyah Rinjani <rarsyansyahr@gmail.com>
2019-10-07 14:16:52 +0700

    Menambahkan Praktikum 1 VB
    Jum'at, 27 September 2019

---
+++ Pemrogaman .NET/praktikum1VB/praktikum1VB/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtnama = New System.Windows.Forms.TextBox()
+        Me.txtnim = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nama"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(27, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "NIM"
+        '
+        'txtnama
+        '
+        Me.txtnama.Location = New System.Drawing.Point(76, 29)
+        Me.txtnama.Name = "txtnama"
+        Me.txtnama.Size = New System.Drawing.Size(157, 20)
+        Me.txtnama.TabIndex = 3
+        '
+        'txtnim
+        '
+        Me.txtnim.Location = New System.Drawing.Point(76, 55)
+        Me.txtnim.Name = "txtnim"
+        Me.txtnim.Size = New System.Drawing.Size(157, 20)
+        Me.txtnim.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.txtnim)
+        Me.Controls.Add(Me.txtnama)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtnama As System.Windows.Forms.TextBox
+    Friend WithEvents txtnim As System.Windows.Forms.TextBox
+
+End Class
---
+++ Pemrogaman .NET/praktikum1VB/praktikum1VB/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtnama.Text = "Rizky Arsyansyah Rinjani"
+        txtnim.Text = "18.11.0153"
+    End Sub
+End Class
